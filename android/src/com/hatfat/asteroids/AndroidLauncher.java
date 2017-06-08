993661840f26cfc833881dc1bcba84f93487ba43
Scott Atkins <scottrick49@gmail.com>
2017-06-08 09:05:36 -0500

    Initial project commit.

---
+++ android/src/com/hatfat/asteroids/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.hatfat.asteroids;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.hatfat.asteroids.Asteroids;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Asteroids(), config);
+	}
+}
